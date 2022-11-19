94027ee198c8dd26fbb174044b41fef8ea887838
chaetesh <chaetesh@gmail.com>
2022-11-19 10:29:11 +0530

    Updates

---
+++ precache-manifest.6aaa49e207997eb273567a25d28f1c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b097fc0ac38d0bc504",
+    "url": "/first-react-app/static/css/main.88e9c73a.chunk.css"
+  },
+  {
+    "revision": "32b097fc0ac38d0bc504",
+    "url": "/first-react-app/static/js/main.32b097fc.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/first-react-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/first-react-app/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "4e5f36ecfda116ed73e9",
+    "url": "/first-react-app/static/js/runtime~main.4e5f36ec.js"
+  },
+  {
+    "revision": "80fa770d715972e64a7c921a9a8d122c",
+    "url": "/first-react-app/index.html"
+  }
+];
